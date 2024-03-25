52fa272bcdb9bafab56b2cb5a83350d0b1447c1b
anonuser <163961703+aeronaut9k@users.noreply.github.com>
2024-03-24 21:24:00 -0700

    Completed files for for assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,39 @@
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+ 
+int main(int argc, char* argv[])
+{
+    openlog(NULL, 0, LOG_USER);
+
+    if(argc < 3)
+    {
+        syslog(LOG_ERR, "Error: File path and text string arguments not provided.");
+        return 1;
+    }
+    else
+    {
+        char* writefile = argv[1];
+        char* writestr = argv[2];
+        syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
+        int fd, nr;
+        fd = creat(writefile, 0644);
+        if(fd != -1)
+        {
+            nr = write(fd, writestr, strlen(writestr));
+        }
+
+        if(fd == -1 || nr != strlen(writestr))
+        {
+            printf("Write fail");
+            syslog(LOG_ERR, "Error writing %s to file %s", writestr, writefile);
+        }
+        close(fd);
+    }
+
+    return 0;
+}
